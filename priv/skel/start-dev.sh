1495adf52885dbdab9534a68f1ace4fa604faf22
Geoff Cant <nem@erlang.geek.nz>
2008-10-14 16:15:56 +0200

    Fixing start-dev.sh to use app config file.

--- priv/skel/start-dev.sh
+++ priv/skel/start-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 exec erl +Bc +K true -smp auto \
-    -conf priv/skel \
+    -config priv/skel \
     -pa $PWD/ebin $PWD/lib/*/ebin \
     -boot start_sasl \
     -name skel \
